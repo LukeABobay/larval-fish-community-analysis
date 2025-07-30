376c3f24a6546b0aa4ab1e4c006f21d21a2d9278
LukeABobay <81708541+LukeABobay@users.noreply.github.com>
2025-07-30 07:59:16 -0700

    Create data wrangling script

---
+++ scripts/01_data_wrangling.R
@@ -0,0 +1,16 @@
+
+# Description -------------------------------------------------------------
+
+# Change winter MEZCAL and SPECTRA larval fish abundance data into a usable
+# form. As of 7/30/2025, these data are incomplete.
+
+
+# Load packages -----------------------------------------------------------
+
+library(here)
+library(tidyverse)
+
+
+# Load data ---------------------------------------------------------------
+
+mocness_2018_02_fish_abundance <- read.csv(here())
