bda5c9dd3ea8e978920eece54700adc9c7ed7c6f
Ross McCloskey <rossmccloskey04@gmail.com>
2025-10-09 17:07:53 -0700

    run permanova with covariates. adjusted where necessary for added columns.

--- scripts/02_fit_statistical_tests.R
+++ scripts/02_fit_statistical_tests.R
@@ -26,38 +26,32 @@ mocness_major_taxa_wide <- mocness_major_taxa %>%
            transect_station, transect, station, latitude_dd, longitude_dd, 
            taxon, transect_station_rep, seafloor_depth_m, shelf_position, 
            prey_zooplankton_abundance_ind_m3, dissolved_oxygen_ml_l, 
-           seawater_density_1000_kg_m3, chlorophyll_ug_l, mlotst) %>%
+           seawater_density_1000_kg_m3, chlorophyll_ug_l, mlotst, 
+           temperature_c, salinity) %>%
   summarize(individuals_in_tow = sum(individuals_in_tow)) %>%
   ungroup() %>%
   pivot_wider(names_from = taxon, values_from = individuals_in_tow, values_fill = 0)
 
 # Create separate community matrix and apply sqrt tranformation
-abundance_by_taxon <- mocness_major_taxa_wide[, 19:41] %>%
+abundance_by_taxon <- mocness_major_taxa_wide[, 21:43] %>%
   sqrt()
 
 # Add tranformed abundances back into main data frame
-mocness_major_taxa_wide_tranformed <- mocness_major_taxa_wide[, 1:18] %>%
+mocness_major_taxa_wide_transformed <- mocness_major_taxa_wide[, 1:20] %>%
   bind_cols(., abundance_by_taxon)
 
 # Try a PERMANOVA
-permanova <- adonis2(abundance_by_taxon ~ seafloor_depth_m + shelf_position, 
-                     data = mocness_major_taxa_wide_tranformed)
+permanova <- adonis2(abundance_by_taxon ~ seafloor_depth_m + shelf_position, data = mocness_major_taxa_wide_tranformed)
 summary(permanova)
 
 ##create a data frame to exclude rows that're missing data for covariates for the time being
-filt_mocness_major_taxa_wide_tranformed <- filter(mocness_major_taxa_wide_tranformed, 
-                                                   prey_zooplankton_abundance_ind_m3 != "" & 
-                                                     dissolved_oxygen_ml_l != "" & 
-                                                     seawater_density_1000_kg_m3 != "" &
-                                                     chlorophyll_ug_l != "" & mlotst != "")
-
-filt_mocness_major_taxa_wide_tranformed <- filter(mocness_major_taxa_wide_tranformed, 
+filt_mocness_major_taxa_wide_transformed <- filter(mocness_major_taxa_wide_tranformed, 
                                                   !is.na(prey_zooplankton_abundance_ind_m3) & 
-                                                    !is.na(dissolved_oxygen_ml_l) & 
-                                                    !is.na(seawater_density_1000_kg_m3) &
-                                                    !is.na(chlorophyll_ug_l != "" & mlotst))
+                                                    !is.na(mlotst))
 
-mult_permanova <- adonis2(filt_mocness_major_taxa_wide_transformed[, 19:41] ~ seafloor_depth_m + shelf_position + depth_range + 
-                            prey_zooplankton_abundance_ind_m3 + dissolved_oxygen_ml_l + 
-                            seawater_density_1000_kg_m3 + chlorophyll_ug_l + mlotst,
-                              data = filt_mocness_major_taxa_wide_tranformed, method = "bray", by = "margin")
+mult_permanova <- adonis2(filt_mocness_major_taxa_wide_transformed[, 21:43] ~ seafloor_depth_m + 
+                            shelf_position + depth_range + prey_zooplankton_abundance_ind_m3 + 
+                            dissolved_oxygen_ml_l + seawater_density_1000_kg_m3 + chlorophyll_ug_l + 
+                            mlotst + temperature_c + salinity,
+                          data = filt_mocness_major_taxa_wide_tranformed, method = "bray", by = "margin")
+view(mult_permanova)
