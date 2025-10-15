a1fd30a7df566aac25b8fcad9d2497f93beb1fcc
Ross McCloskey <rossmccloskey04@gmail.com>
2025-10-14 17:35:15 -0700

    add transect_station_rep_year identifier to mocness_major_taxa_wide

--- scripts/02_fit_statistical_tests.R
+++ scripts/02_fit_statistical_tests.R
@@ -24,8 +24,8 @@ mocness_major_taxa_wide <- mocness_major_taxa %>%
   filter(!is.na(individuals_in_tow)) %>%
   group_by(project, collection_date, time, replicate, depth_range, 
            transect_station, transect, station, latitude_dd, longitude_dd, 
-           taxon, transect_station_rep, seafloor_depth_m, shelf_position, 
-           prey_zooplankton_abundance_ind_m3, dissolved_oxygen_ml_l, 
+           taxon, transect_station_rep, transect_station_rep_year, seafloor_depth_m, 
+           shelf_position,prey_zooplankton_abundance_ind_m3, dissolved_oxygen_ml_l, 
            seawater_density_1000_kg_m3, chlorophyll_ug_l, mlotst, 
            temperature_c, salinity) %>%
   summarize(individuals_in_tow = sum(individuals_in_tow)) %>%
