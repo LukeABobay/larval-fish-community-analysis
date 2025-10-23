af6308525171d4e8ca9d7426af426d76c62af404
Ross McCloskey <rossmccloskey04@gmail.com>
2025-10-23 13:30:39 -0700

    Create scatterplot and correlation coefficient matrices for environmental covariates with GGally package

--- scripts/05_plot_scatter_visuals.R
+++ scripts/05_plot_scatter_visuals.R
@@ -29,15 +29,24 @@ env_covariates_wide$depth_range <- as.factor(env_covariates_wide$depth_range)
 env_covariates_wide$shelf_position <- as.factor(env_covariates_wide$shelf_position)
 
 #create scatter plot matrix of covariates
-
+##base R method
 pairs(env_covariates_wide[,6:15], lower.panel = NULL, 
       main = "Scatterplots of MOCNESS environmental covariates",
       labels = c("depth range sampled", "shelf position", "seafloor depth", "prey abundance", 
                  "dissolved oxygen", "seawater density", "[chlorophyll]", "mixed layer depth", 
                  "temperature", "salinity"))
-
 #view correlation coefficients for continuous variables
 correlations <- cor(env_covariates_wide[,8:15])
 
-##note from ross: will go back next week and try to plot with ggally to better plot
-##the scatter plot matrix
+##GGPlot method using GGally
+##ggpairs scatterplot matrices
+ggpairs(env_covariates_wide, columns = 6:15)
+##remove density plots in diagonal
+ggpairs(env_covariates_wide, columns = 6:15, diag = list(continuous = "blankDiag"))
+##remove categorical variables and add one (shelf position) represented by colors
+ggpairs(env_covariates_wide, columns = 8:15, aes(color = shelf_position), 
+        upper = list(continuous = wrap("cor", size = 2.5)))
+##remove density plots in diagonal
+ggpairs(env_covariates_wide, columns = 8:15, diag = list(continuous = "blankDiag"), 
+        aes(color = shelf_position), upper = list(continuous = wrap("cor", size = 2.5)))
+
