e317189f8a23e931326329c4748dc736c2bf487c
Ross McCloskey <rossmccloskey04@gmail.com>
2026-02-05 15:11:33 -0800

    Switch to unfiltered dataframe for analysis.

--- scripts/07_analyses_by_net.R
+++ scripts/07_analyses_by_net.R
@@ -22,14 +22,14 @@ source(here("scripts/01_data_wrangling.R"))
 
 
 # Create wide environmental dataframe ---------------------------------------------------
-volume_sampled_by_both_sides <- mocness_major_taxa_stations %>%
+volume_sampled_by_both_sides <- mocness_major_taxa %>%
   distinct(transect_station_rep_year, mocness_side, net, .keep_all = TRUE) %>%
   group_by(transect_station_rep_year, net) %>%
   mutate(combined_volume_m3_best = sum(volume_m3_best)) %>%
   ungroup() %>%
   distinct(transect_station_rep_year, net, combined_volume_m3_best)
 
-nets_major_taxa_wide <- mocness_major_taxa_stations %>%
+nets_major_taxa_wide <- mocness_major_taxa %>%
   # Removing NAs for now, but there shouldn't be any to begin with
   filter(!is.na(individuals_in_tow)) %>%
   filter(!is.na(individuals_per_m3)) %>%
