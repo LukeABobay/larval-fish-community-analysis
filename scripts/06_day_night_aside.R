6f37a83201924c4071dd5349919dee4576c2458d
Ross McCloskey <rossmccloskey04@gmail.com>
2026-02-05 12:29:22 -0800

    Adjust taxa in habitat affinity groups.

--- scripts/06_day_night_aside.R
+++ scripts/06_day_night_aside.R
@@ -29,17 +29,16 @@ mocness_major_taxa_19 <- filter(mocness_major_taxa, collection_date > "2019-01-0
 
 # Classify taxa by habitat affinity and create color vectors ---------------
 #Categories of species with >15 individuals from MEZCAL
-coastal_species <- c("Agonidae", "Ammodytes", "Anarrhichthys ocellatus", "Artedius", "Chilara taylori", "Cottidae", "Cryptacanthodes",
-                     "Gadidae", "Gobiidae", "Hexagrammidae", "Liparis", "Osmeridae", "Paralichthyidae", "Parophrys vetulus", 
-                     "Pholidae", "Pleuronectidae", "Ptilichthys goodei", "Ronquilus jordani", "Sebastes", "Stichaeidae")
+coastal_species <- c("Agonidae", "Ammodytidae", "Anarrhichthys_ocellatus", "Artedius", "Cottidae", "Cryptacanthodes",
+                     "Gadidae", "Hexagrammidae", "Liparis", "Paralichthyidae", "Parophrys_vetulus", "Alepocephalidae",
+                     "Pholidae", "Pleuronectidae_other", "Sebastes", "Stichaeidae", "Bathymasteridae")
 coastal_colors <- colorRampPalette(brewer.pal(9, "Greens")[2:9])(length(coastal_species))
 
-coastal_oceanic_species <- c("Anoplopomatidae", "Engraulis mordax", "Lestidiops ringens", "Sardinops sagax", 
-                             "Sebastolobus")
+coastal_oceanic_species <- c("Anoplopomatidae", "Engraulis_mordax", "Clupeidae_other") 
 coastal_oceanic_colors <- colorRampPalette(brewer.pal(9, "Blues")[2:9])(length(coastal_oceanic_species))
 
-oceanic_species <- c("Bathylagus pacificus", "Chauliodontidae", "Chauliodus macouni", "Lipolagus ochotensis", "Macrouridae", 
-                     "Merluccius productus", "Myctophidae", "Nansenia candida", "Trachipterus altivelis")
+oceanic_species <- c("Chauliodus_macouni", "Lipolagus_ochotensis", "Macrouridae", "Paraleptidae",
+                     "Merluccius_productus", "Myctophidae", "Nansenia_candida", "Bathylagidae")
 oceanic_colors <- colorRampPalette(brewer.pal(9, "Purples")[2:9])(length(oceanic_species))
 
 #Named species color vector
@@ -48,11 +47,11 @@ species_colors <- c(setNames(coastal_colors, coastal_species),
                     setNames(oceanic_colors, oceanic_species))
 
 #Vector of taxa ordered alphabetically within categories to order bars and figure legends
-ordered_taxa <- c(coastal_species, coastal_oceanic_species, oceanic_species)
+ordered_taxa_19 <- c(coastal_species, coastal_oceanic_species, oceanic_species)
 
 mocness_major_taxa_19 <- mocness_major_taxa_19 %>%
   #Reorder taxa
-  mutate(taxon = factor(taxon, levels = ordered_taxa)) %>%
+  mutate(taxon = factor(taxon, levels = ordered_taxa_19)) %>%
   #Reorder stations
   mutate(station = factor(station, levels = rev(sort(unique(station))))) %>%
   mutate(adult_habitat_affinity = case_when(taxon %in% coastal_species ~ "Coastal",
