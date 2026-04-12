ca8973422a272db5fd9aec07657d2c58c7bed9e3
LukeABobay <81708541+LukeABobay@users.noreply.github.com>
2026-04-12 15:58:26 -0700

    Apply Hellinger transformation and use concentration data instead of count data

--- scripts/07_analyses_by_net.R
+++ scripts/07_analyses_by_net.R
@@ -22,48 +22,34 @@ source(here("scripts/01_data_wrangling.R"))
 
 
 # Create wide environmental dataframe ---------------------------------------------------
-volume_sampled_by_both_sides <- mocness_major_taxa_nets %>%
-  distinct(transect_station_rep_year, mocness_side, net, .keep_all = TRUE) %>%
-  group_by(transect_station_rep_year, net) %>%
-  mutate(combined_volume_m3_best = sum(volume_m3_best)) %>%
-  ungroup() %>%
-  distinct(transect_station_rep_year, net, combined_volume_m3_best)
 
 nets_major_taxa_wide <- mocness_major_taxa_nets %>%
   # Removing NAs for now, but there shouldn't be any to begin with
   filter(!is.na(individuals_in_tow)) %>%
   filter(!is.na(individuals_per_m3)) %>%
-  merge(., volume_sampled_by_both_sides, by = c("transect_station_rep_year", "net"), all.x = TRUE) %>%
-  distinct(transect_station_rep_year_net, taxon, .keep_all = TRUE) %>%
-  mutate(avg_taxa_conc = individuals_in_tow_both_sides/combined_volume_m3_best) %>%
-  select(project, cruise, year, collection_date, transect, replicate, station, net, 
-         transect_station_rep_year, transect_station_rep_year_net, start_time_pt, 
-         start_longitude_dd, start_latitude_dd,
-         maximum_depth_m, minimum_depth_m, depth_mean_m, depth_diff_m,
+  select(project, year, cruise, collection_date, transect, replicate, station, net,
+         transect_station_rep_year_net, transect_station_rep_year, start_time_pt,
+         start_longitude_dd, start_latitude_dd, maximum_depth_m, minimum_depth_m, 
+         depth_mean_m, depth_diff_m, volume_best_m3_both_sides,
          mean_temperature_c, mean_salinity_psu, mean_density_kgm3, seafloor_depth_m,
          distance_to_shore_km, shelf_position, prey_zooplankton_abundance_ind_m3,
-         dissolved_oxygen_ml_l, mean_chl_0_100_m_mgm3, mlotst, taxon, individuals_in_tow_both_sides,
-         combined_volume_m3_best) %>%
+         dissolved_oxygen_ml_l, mean_chl_0_100_m_mgm3, mlotst, 
+         taxon, individuals_per_m3) %>%
   # For some reason, MOC 1 and MOC 4 have different values of mean_temperature_c, mean_salinity_psu, and mean_density_kgm3 in 6 cases. To eliminate differences, calculate mean
   group_by(transect_station_rep_year_net) %>%
   mutate(mean_temperature_c = mean(mean_temperature_c),
          mean_salinity_psu = mean(mean_salinity_psu),
          mean_density_kgm3 = mean(mean_density_kgm3)) %>%
   ungroup() %>%
-  pivot_wider(names_from = taxon, values_from = individuals_in_tow_both_sides, values_fill = 0)
+  pivot_wider(names_from = taxon, values_from = individuals_per_m3, values_fill = 0)
 
 nets_env_wide <- nets_major_taxa_wide %>%
-  select(project, cruise, collection_date, year, replicate, transect_station_rep_year_net, start_time_pt,
-         start_latitude_dd, start_longitude_dd, 
-         maximum_depth_m, minimum_depth_m, depth_mean_m, depth_diff_m, shelf_position,
-         seafloor_depth_m, dissolved_oxygen_ml_l, distance_to_shore_km, mean_density_kgm3, 
-         mean_chl_0_100_m_mgm3, mean_temperature_c, mean_salinity_psu, combined_volume_m3_best) %>%
   mutate(time_of_day = substr(replicate, 3, 3)) %>%
   mutate(time_of_day = recode(time_of_day, "D" = "Day", "N" = "Night")) %>%
   mutate(time_of_day = factor(time_of_day, levels = c("Day", "Night"))) %>%
   select(project, collection_date, year, transect_station_rep_year_net, time_of_day, start_time_pt,
          start_latitude_dd, shelf_position, seafloor_depth_m, dissolved_oxygen_ml_l, 
-         mean_temperature_c, mean_salinity_psu, depth_mean_m, combined_volume_m3_best)
+         mean_temperature_c, mean_salinity_psu, depth_mean_m, volume_best_m3_both_sides)
 #removed mlotst and prey abundance for right now because both have NAs at the moment and I don't want this to cause errors down the line
 #also excluded redundant information like transect, transect_station, transect_station_rep, and so on. Included only variables of interest
 # removed chlorophyll until we have values for fluorescence
@@ -71,8 +57,13 @@ nets_env_wide <- nets_major_taxa_wide %>%
 
 # Perform cluster analysis ------------------------------------------------
 
+# Apply Hellinger transformation
 nets_AHC_comm_matrix <- nets_major_taxa_wide %>%
-  select(transect_station_rep_year_net, depth_mean_m, 29:64)
+  select(transect_station_rep_year_net, depth_mean_m) %>%
+  bind_cols(nets_major_taxa_wide %>%
+      select(29:50) %>%
+      decostand(method = "hellinger") %>%
+      as.data.frame())
 
 nets_transform_taxa_concentrations <- nets_AHC_comm_matrix[, 3:38] %>%
   sqrt()
