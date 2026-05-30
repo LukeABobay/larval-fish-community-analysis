92642e9d6ed0d6272cb5f6dc4458101c8da584bd
Ross McCloskey <rossmccloskey04@gmail.com>
2026-05-30 16:41:54 -0700

    create new scripts in which to run the objective 2 and 3 analyses and plots on only the 4 main clusters.

---
+++ scripts/10_obj_3_main_clusters.R
@@ -0,0 +1,16 @@
+# Description -------------------------------------------------------------
+
+#Run objective 3 analyses and plots with only 4 main clusters
+
+# Load packages -----------------------------------------------------------
+
+library(here)
+library(tidyverse)
+library(ggplot2)
+library(RColorBrewer)
+library(dplyr)
+
+
+# Source code -------------------------------------------------------------
+
+source(here("scripts/01_data_wrangling.R"))
