296c6305d58ca891a508108343ecd1d0408728ea
LukeABobay <81708541+LukeABobay@users.noreply.github.com>
2026-08-13 13:49:52 -0700

    Update for consistency with new daytime variable

--- scripts/02_fit_statistical_tests.R
+++ scripts/02_fit_statistical_tests.R
@@ -23,7 +23,7 @@ mocness_major_taxa_wide <- mocness_major_taxa %>%
   # Removing NAs for now, but there shouldn't be any to begin with
   filter(!is.na(individuals_in_tow)) %>%
   filter(!is.na(individuals_per_m3)) %>%
-  group_by(year, project, collection_date, start_time_pt, replicate, depth_range, maximum_depth_m,
+  group_by(year, project, collection_date, start_time_pt, solar_dayness, replicate, depth_range, maximum_depth_m,
            minimum_depth_m, depth_mean_m, depth_diff_m,transect_station, transect, station, 
            start_latitude_dd, start_longitude_dd, taxon, transect_station_rep, 
            transect_station_rep_year, seafloor_depth_m, shelf_position,
@@ -34,12 +34,24 @@ mocness_major_taxa_wide <- mocness_major_taxa %>%
   ungroup() %>%
   pivot_wider(names_from = taxon, values_from = individuals_per_m3, values_fill = 0)
 
+permanova_metadata_cols <- c("year", "project", "collection_date", "start_time_pt", "solar_dayness",
+                             "replicate", "depth_range", "maximum_depth_m", "minimum_depth_m",
+                             "depth_mean_m", "depth_diff_m", "transect_station", "transect", "station",
+                             "start_latitude_dd", "start_longitude_dd", "transect_station_rep",
+                             "transect_station_rep_year", "seafloor_depth_m", "shelf_position",
+                             "prey_zooplankton_abundance_ind_m3", "dissolved_oxygen_ml_l",
+                             "seawater_density_1000_kg_m3", "chlorophyll_ug_l", "mlotst",
+                             "mean_temperature_c", "mean_salinity_psu")
+permanova_taxa_cols <- setdiff(names(mocness_major_taxa_wide), permanova_metadata_cols)
+
 # Create separate community matrix and apply sqrt tranformation
-concentration_by_taxon <- mocness_major_taxa_wide[, 27:61] %>%
+concentration_by_taxon <- mocness_major_taxa_wide %>%
+  select(all_of(permanova_taxa_cols)) %>%
   sqrt()
 
 # Add tranformed abundances back into main data frame
-mocness_major_taxa_wide_transformed <- mocness_major_taxa_wide[, 1:26] %>%
+mocness_major_taxa_wide_transformed <- mocness_major_taxa_wide %>%
+  select(all_of(permanova_metadata_cols)) %>%
   bind_cols(., concentration_by_taxon)
 
 # Try a PERMANOVA
@@ -49,14 +61,11 @@ summary(permanova)
 ##create a data frame to exclude rows that're missing data for covariates for the time being
 ##right now this filters out all rows?? so I'm taking out the mlotst line for now and not including it in multiple permanova
 filt_mocness_major_taxa_wide_transformed <- filter(mocness_major_taxa_wide_transformed, 
-                                                  !is.na(prey_zooplankton_abundance_ind_m3)) %>%
-  mutate(time_of_day = substr(replicate, 3, 3)) %>%
-  mutate(time_of_day = recode(time_of_day, "D" = "Day", "N" = "Night")) %>%
-  mutate(time_of_day = factor(time_of_day, levels = c("Day", "Night")))
+                                                  !is.na(prey_zooplankton_abundance_ind_m3))
                                                     #& !is.na(mlotst))
 
-mult_permanova <- adonis2(filt_mocness_major_taxa_wide_transformed[, 27:61] ~ mean_temperature_c * mean_salinity_psu +
-                            year + dissolved_oxygen_ml_l + time_of_day + start_latitude_dd + depth_mean_m + 
+mult_permanova <- adonis2(filt_mocness_major_taxa_wide_transformed[, permanova_taxa_cols] ~ mean_temperature_c * mean_salinity_psu +
+                            year + dissolved_oxygen_ml_l + solar_dayness + start_latitude_dd + depth_mean_m + 
                             seafloor_depth_m,
                           data = filt_mocness_major_taxa_wide_transformed, method = "bray", by = "margin")
 #not currently including mlotst and chlorophyll/flurosence till filled in
--- scripts/04_explore_LFC_and_covariates.R
+++ scripts/04_explore_LFC_and_covariates.R
@@ -30,32 +30,44 @@ mocness_major_taxa_wide <- mocness_major_taxa %>%
   # Removing NAs for now, but there shouldn't be any to begin with
   filter(!is.na(individuals_in_tow)) %>%
   filter(!is.na(individuals_per_m3)) %>%
-  group_by(project, collection_date, start_time_pt, depth_range, transect_station_rep_year,
+  group_by(project, collection_date, start_time_pt, solar_dayness, depth_range, transect_station_rep_year,
            start_latitude_dd, start_longitude_dd, taxon, seafloor_depth_m, 
            distance_to_shore_km, shelf_position,prey_zooplankton_abundance_ind_m3, dissolved_oxygen_ml_l, 
            seawater_density_1000_kg_m3, chlorophyll_ug_l, mlotst,mean_temperature_c, mean_salinity_psu) %>%
   summarize(individuals_per_m3 = sum(individuals_per_m3)) %>%
   ungroup() %>%
-  pivot_wider(names_from = taxon, values_from = individuals_per_m3, values_fill = 0)
+  pivot_wider(names_from = taxon, values_from = individuals_per_m3, values_fill = 0) %>%
+  mutate(sample_id = as.character(row_number()))
+
+explore_metadata_cols <- c("sample_id", "project", "collection_date", "start_time_pt", "solar_dayness",
+                           "depth_range", "transect_station_rep_year",
+                           "start_latitude_dd", "start_longitude_dd", "seafloor_depth_m",
+                           "distance_to_shore_km", "shelf_position", "prey_zooplankton_abundance_ind_m3",
+                           "dissolved_oxygen_ml_l", "seawater_density_1000_kg_m3",
+                           "chlorophyll_ug_l", "mlotst", "mean_temperature_c", "mean_salinity_psu")
+explore_taxa_cols <- setdiff(names(mocness_major_taxa_wide), explore_metadata_cols)
 
 ##split community matrix and environmental metadata into separate data frames
 comm_matrix <- mocness_major_taxa_wide %>%
-  select(-project, -collection_date, -start_time_pt, -depth_range,
-         -start_latitude_dd, -start_longitude_dd, -seafloor_depth_m, -distance_to_shore_km,
-         -shelf_position, -prey_zooplankton_abundance_ind_m3, -dissolved_oxygen_ml_l, 
-         -seawater_density_1000_kg_m3, -chlorophyll_ug_l, -mlotst, -mean_temperature_c, -mean_salinity_psu)
+  select(sample_id, all_of(explore_taxa_cols))
 env_meta <- mocness_major_taxa_wide %>%
-  select(project, collection_date, start_time_pt, depth_range, transect_station_rep_year,
+  select(sample_id, project, collection_date, start_time_pt, solar_dayness, depth_range, transect_station_rep_year,
          start_latitude_dd, start_longitude_dd, seafloor_depth_m, distance_to_shore_km,
          shelf_position, prey_zooplankton_abundance_ind_m3, dissolved_oxygen_ml_l, 
          seawater_density_1000_kg_m3, chlorophyll_ug_l, mlotst, mean_temperature_c, mean_salinity_psu)
 
 ##transform standardized taxa counts and run bray-curtis on community matrix
-transform_concentrations <- comm_matrix[, 2:24] %>%
+transform_concentrations <- comm_matrix %>%
+  select(all_of(explore_taxa_cols)) %>%
   sqrt()
-comm_matrix_transformed <- comm_matrix[,1] %>%
+comm_matrix_transformed <- comm_matrix %>%
+  select(sample_id) %>%
   bind_cols(.,transform_concentrations)
-dissim_matrix <- vegdist(comm_matrix_transformed[,2:24], method = "bray")
+comm_matrix_for_dist <- comm_matrix_transformed %>%
+  select(all_of(explore_taxa_cols)) %>%
+  as.data.frame()
+row.names(comm_matrix_for_dist) <- comm_matrix_transformed$sample_id
+dissim_matrix <- vegdist(comm_matrix_for_dist, method = "bray")
 
 ##bin environmental data for plotting
 env_meta_binned <- env_meta %>%
@@ -64,10 +76,7 @@ env_meta_binned <- env_meta %>%
     prey_abundance_bin = cut(prey_zooplankton_abundance_ind_m3, breaks = quantile(prey_zooplankton_abundance_ind_m3, probs = seq(0, 1, 0.25), na.rm = TRUE), labels = c("1", "2", "3", "4"), include.lowest = TRUE),
     DO_bin = cut(dissolved_oxygen_ml_l, breaks = quantile(dissolved_oxygen_ml_l, probs = seq(0, 1, 0.25), na.rm = TRUE), labels = c("1", "2", "3", "4"), include.lowest = TRUE),
     density_bin = cut(seawater_density_1000_kg_m3, breaks = quantile(seawater_density_1000_kg_m3, probs = seq(0, 1, 0.25), na.rm = TRUE), labels = c("1", "2", "3", "4"), include.lowest = TRUE),
-    chlorophyll_bin = cut(chlorophyll_ug_l, breaks = quantile(chlorophyll_ug_l, probs = seq(0, 1, 0.25), na.rm = TRUE), labels = c("1", "2", "3", "4"), include.lowest = TRUE),
-    time_parsed = as.POSIXct(start_time_pt, format =  "%H:%M:%S", tz = "America/Los_Angeles"),
-    hour = as.numeric(format(time_parsed, "%H")),
-    day_night = case_when(hour>=6 & hour<18 ~ "day", TRUE ~ "night"))
+    chlorophyll_bin = cut(chlorophyll_ug_l, breaks = quantile(chlorophyll_ug_l, probs = seq(0, 1, 0.25), na.rm = TRUE), labels = c("1", "2", "3", "4"), include.lowest = TRUE))
 
 #all mlotst are currently NA so no data frame can be made filtering by this so I'm hiding these parts for now
 ##create separate 2018-2019 community matrix filtered by the presence of a mlotst value
@@ -105,12 +114,25 @@ boxplot(bd_shelf_position$distances ~ bd_shelf_position$group,
         ylab = "Distance to Group Centroid (Bray-Curtis)",
         main = "Larval Fish Community Dispersion by Shelf Position")
 
-##box plot of bray-curtis dissimilarity by time (day vs night)
-bd_time <- betadisper(dissim_matrix, group = env_meta_binned$day_night)
-boxplot(bd_time$distances ~ bd_time$group,
-        xlab = "Time of Sample",
-        ylab = "Distance to Group Centroid (Bray-Curtis)",
-        main = "Larval Fish Community Dispersion by Sample Time")
+##plot average Bray-Curtis dissimilarity by solar dayness
+mean_bray_by_sample <- as.matrix(dissim_matrix) %>%
+  as.data.frame() %>%
+  rownames_to_column("sample_id") %>%
+  pivot_longer(-sample_id,
+               names_to = "comparison_sample",
+               values_to = "bray_curtis") %>%
+  filter(sample_id != comparison_sample) %>%
+  group_by(sample_id) %>%
+  summarise(mean_bray_curtis = mean(bray_curtis, na.rm = TRUE), .groups = "drop") %>%
+  left_join(env_meta_binned, by = "sample_id")
+
+ggplot(mean_bray_by_sample, aes(x = solar_dayness, y = mean_bray_curtis)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +
+  theme_classic() +
+  labs(x = "Solar dayness",
+       y = "Mean Bray-Curtis dissimilarity",
+       title = "Larval Fish Community Dissimilarity by Solar Dayness")
 
 ##box plot of bray-curtis dissimilarity by seafloor depth
 bd_seafloor_depth <- betadisper(dissim_matrix, group = env_meta_binned$seafloor_bin)
